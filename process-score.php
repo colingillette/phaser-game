01dfff98b72911c07faac77a311b6d743ea1ba13
Colin Gillette <colingillette35@gmail.com>
2019-04-29 19:51:50 -0500

    begin form implementation

--- process-score.php
+++ process-score.php
@@ -1,5 +1,16 @@
 <?php
-    
+  if ($_SERVER["REQUEST_METHOD"] == "POST")
+  {
+    $display = test_input($_POST["displayname"]);
+  }
 
-    header('Location: index.html');
+  header('Location: index.html');
+
+  function test_input($data)
+  {
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
+  }
 ?>
